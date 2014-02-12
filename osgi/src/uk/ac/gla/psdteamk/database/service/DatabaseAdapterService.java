81877f171d007a21324ee2c132ddf43255c67be7
HelenFoster <0904275f@student.gla.ac.uk>
2014-02-12 20:41:26 +0000

    Database adapter bundle.

--- osgi/src/uk/ac/gla/psdteamk/database/service/DatabaseAdapterService.java
+++ osgi/src/uk/ac/gla/psdteamk/database/service/DatabaseAdapterService.java
@@ -1,5 +1,16 @@
 package uk.ac.gla.psdteamk.database.service;
 
-public interface DatabaseAdapterService {
+import java.sql.Connection;
+import java.util.ArrayList;
 
+import uk.ac.gla.psdteamk.objects.*;
+
+public interface DatabaseAdapterService {
+	
+	public Connection getConnection(); //??????
+	public ArrayList<Course> getAllCourses();
+	public ArrayList<Session> getAllSessions();
+	public Course getCourse(int id);
+	
+	
 }
