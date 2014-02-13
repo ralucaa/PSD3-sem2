241e9c991b9d217e8105a34882a86f68e6f316d3
Tomasz Sadowski <1106113s@student.gla.ac.uk>
2014-02-13 01:24:24 +0000

    check session details done

--- ge2/src/functions/CheckSessionDetails.java
+++ ge2/src/functions/CheckSessionDetails.java
@@ -59,50 +59,58 @@ public class CheckSessionDetails {
 			//Execute the statement and get the result.
 			resultSet = statement.executeQuery();
 			//crunch
-			System.out.println("Courses you are registered in:");
+			System.out.println("Session info:");
 			ArrayList<String> tutors = new ArrayList<String>();
 			boolean toggle = true;
 			boolean isLab = false;
 			while(resultSet.next()){
 				//got a lecture
 				if(resultSet.getString(8) == null){
-					System.out.println(resultSet.getString(1)+resultSet.getDate(2)+resultSet.getTime(3)+resultSet.getTime(4)+resultSet.getInt(5)+
-						resultSet.getInt(6)+resultSet.getString(7));
+					System.out.println("First session on "+ resultSet.getString(3)+
+							"\nfrom "+resultSet.getString(4)+" to "+resultSet.getString(5)+"\nfrequency "+resultSet.getString(6)+
+							"\nroom "+resultSet.getInt(7)+ "\ncapacity "+resultSet.getInt(8)+"\ntype: "+resultSet.getString(9));
 				}
 				//got a lab with tutors
 				else{
 					isLab = true;
 					//enter here only once to display lab session info
 					if(toggle == true){
-						System.out.println(resultSet.getString(1)+resultSet.getDate(2)+resultSet.getTime(3)+resultSet.getTime(4)+resultSet.getInt(5)+
-								resultSet.getInt(6)+resultSet.getString(7));
+						System.out.println("First session on "+resultSet.getString(3)+
+								"\nfrom "+resultSet.getString(4)+" to "+resultSet.getString(5)+"\nfrequency "+resultSet.getString(6)+
+								"\nroom "+resultSet.getInt(7)+"\ncapacity "+resultSet.getInt(8)+"\ntype: "+resultSet.getString(9));
 					}
 					toggle = false;
-					tutors.add(resultSet.getString(8));
+					tutors.add(resultSet.getString(10));
 				}	
 			}
+			statement.close();
 			//session info displayed. if it was a lab session then tutor matric numbers added into the tutor ArrayList
 			//If it was a lab session, print the tutors
 			if(isLab){
-				System.out.println("Tutors in this session:");
-				String tutorQuery = "SELECT name FROM User WHERE name =?";
+				System.out.println("\nTutors in this session:");
+				String tutorQuery = "SELECT name FROM User WHERE guid = ?";
 				Connection myCampusCon = MyCampusDatabaseAdapter.getConnection();
 				//Prepare the SQL statement.
 				PreparedStatement tutorStatement = myCampusCon.prepareStatement(tutorQuery);
 				ResultSet tutorResultSet = null;
 				for(String matric: tutors){
-					tutorStatement.setInt(1, Integer.parseInt(matric));
-					tutorResultSet = statement.executeQuery();
+					tutorStatement.setString(1, matric);
+					tutorResultSet = tutorStatement.executeQuery();
 					tutorResultSet.next();
-					System.out.println(tutorResultSet.getString(1));
+					try {
+						System.out.println(tutorResultSet.getString(1));
+					} catch (SQLException e) {
+						System.out.println("no tutors");
+						break;
+					}
 				}
 				
 				try {
 					if (tutorStatement != null){
-						statement.close();
+						tutorStatement.close();
 					}
 					if (myCampusCon != null){
-						con.close();
+						myCampusCon.close();
 					}
 				}
 				catch (SQLException ex){
@@ -111,7 +119,34 @@ public class CheckSessionDetails {
 			}
 			//////////////////////////////////////////////////////////////////
 			//print all the students
-			String studentQuery = "";
+			String studentsQuery = "SELECT student "+
+									"FROM Session,Registration "+
+									"WHERE Session.id = Registration.session AND Session.id =?";
+			statement = con.prepareStatement(studentsQuery);
+			statement.setInt(1, Integer.parseInt(sessionID));
+			resultSet = statement.executeQuery();
+			ArrayList<String> students = new ArrayList<String>();
+			while(resultSet.next()){
+				students.add(resultSet.getString(1));
+			}
+			statement.close();
+			System.out.println("\nStudents in this session:");
+			studentsQuery = "SELECT name FROM User WHERE guid = ?";
+			Connection myCampusCon = MyCampusDatabaseAdapter.getConnection();
+			//Prepare the SQL statement.
+			statement = myCampusCon.prepareStatement(studentsQuery);
+			resultSet = null;
+			for(String matric: students){
+				statement.setString(1, matric);
+				resultSet = statement.executeQuery();
+				resultSet.next();
+				try {
+					System.out.println(resultSet.getString(1) +" "+matric);
+				} catch (SQLException e) {
+					System.out.println("no students in this session");
+				}
+			}
+			myCampusCon.close();
 			
 					
 			System.out.println("\nQuery successfull!");
@@ -124,6 +159,7 @@ public class CheckSessionDetails {
 				if (statement != null){
 					statement.close();
 				}
+				
 				if (con != null){
 					con.close();
 				}
