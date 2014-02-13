357c721cbfabebb4da8149ec0c8d075b7d577bb6
HelenFoster <0904275f@student.gla.ac.uk>
2014-02-13 09:54:36 +0000

    Trying to work out how to connect bundles.

--- osgi/src/uk/ac/gla/psdteamk/sessions/Activator.java
+++ osgi/src/uk/ac/gla/psdteamk/sessions/Activator.java
@@ -2,12 +2,17 @@ package uk.ac.gla.psdteamk.sessions;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
 
+import uk.ac.gla.psdteamk.database.service.DatabaseAdapterService;
+import uk.ac.gla.psdteamk.mycampus.service.MyCampusService;
 
 public class Activator implements BundleActivator {
     public void start(BundleContext context)
     {
-
+    	ServiceReference dbRef = context.getServiceReference(DatabaseAdapterService.class.getName());
+    	ServiceReference mcRef = context.getServiceReference(MyCampusService.class.getName());
+     
     }
     
     public void stop(BundleContext context)
