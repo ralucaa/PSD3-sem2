d3863f3e4215d6224724a9a97360b33279dd9006
Tomasz Sadowski <qba19922@gmail.com>
2014-02-25 18:35:40 +0000

    suicide

---
+++ osgi/src/uk/ac/gla/psdteamk/sessions/steps/ImportMyCampusCourse.java
@@ -0,0 +1,26 @@
+package uk.ac.gla.psdteamk.sessions.steps;
+
+import org.jbehave.core.annotations.Given;  
+import org.jbehave.core.annotations.Named;  
+import org.jbehave.core.annotations.Then;  
+import org.jbehave.core.annotations.When;  
+
+
+public class ImportMyCampusCourse {
+	
+	  @Given("a valid MyCampus course ID$")  
+	  public void givenAMyCampusID() {  
+	    
+	  }  
+	
+	  @When("I try to retrieve the course information ")  
+	  public void whenIAddNumber1AndNumber2() {  
+	     
+	  }  
+	
+	  @Then("a function should accept this course ID AND the function should return the sessions for this course")  
+	  public void thenTheOutcomeShould(@Named("sessions")String sessions) {  
+	    
+	  }  
+}
+
