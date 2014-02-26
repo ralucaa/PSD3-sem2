f9b245884e3dc13435f4530694469a42a91d864d
ralucaa <1102430c@student.gla.ac.uk>
2014-02-26 12:26:59 +0000

    Added test cases for user story #8

---
+++ osgi/src/uk/ac/gla/psdteamk/sessions/test/steps/AssignRoomToTimetableSlotSteps.java
@@ -0,0 +1,48 @@
+package uk.ac.gla.psdteamk.sessions.test.steps;
+
+
+import org.jbehave.core.annotations.Given;
+import org.jbehave.core.annotations.Then;
+import org.jbehave.core.annotations.When;
+import org.jbehave.core.steps.Steps;
+
+import uk.ac.gla.psdteamk.objects.Session;
+import uk.ac.gla.psdteamk.sessions.service.SessionManagerService;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Narrative: 
+ * In order to have room bookings recorded 
+ * As an administrator 
+ * I want to assign a room to a timetable slot
+ *  
+*/
+public class AssignRoomToTimetableSlotSteps extends Steps {
+	private SessionManagerService service;
+	private boolean result;
+	private int sessionId;
+	private int roomId;
+	
+	@Given("a $sessionId and $roomId")
+	public void validSessionRoomId(int sessionId, int roomId){
+		this.sessionId=sessionId;
+		this.roomId=roomId;	
+	}
+	
+	@When("the $assignRoom method is executed")
+	public void assignRoom(){
+		result=service.assignRoom(sessionId, roomId);
+	}
+	
+	@Then("the method returns true")
+	public void returnResultTrue(){
+		assertEquals(true,result);
+	}
+	
+	@Then("the method returns false")
+	public void returnResultFalse(){
+		assertEquals(false,result);
+	}
+	
+	
+}
