c00c9b66c6b719477e3a44ecc773ba643a2a2560
Tomazz <1106113s@student.gla.ac.uk>
2014-02-27 13:58:34 +0000

    Update CheckSessionDetailsSteps.java

--- osgi/src/uk/ac/gla/psdteamk/sessions/test/steps/CheckSessionDetailsSteps.java
+++ osgi/src/uk/ac/gla/psdteamk/sessions/test/steps/CheckSessionDetailsSteps.java
@@ -1,5 +1,37 @@
 package uk.ac.gla.psdteamk.sessions.test.steps;
 
-public class CheckSessionDetailsSteps {
+import static org.junit.Assert.assertEquals;
 
+import org.jbehave.core.annotations.Given;  
+import org.jbehave.core.annotations.Named;  
+import org.jbehave.core.annotations.Then;  
+import org.jbehave.core.annotations.When;  
+
+import uk.ac.gla.psdteamk.database.service.DatabaseAdapterService;
+import uk.ac.gla.psdteamk.objects.Course;
+import uk.ac.gla.psdteamk.objects.Session;
+import uk.ac.gla.psdteamk.sessions.*;
+
+import org.jbehave.core.steps.Steps;
+
+public class CheckSessionDetailsSteps extends Steps{
+	private boolean detailsChecked;
+	private DatabaseAdapterService da;
+	private Session session;
+		
+	  @Given("Given a session $s")  
+	  public void givenASession(String s) {  
+	    this.session = new Session();
+	  }  
+	
+	  @When("When I want to check the session details")  
+	  public void whenIcheck() {  
+	      //check details
+		  detailsChecked = true;
+	  }  
+	
+	  @Then("a function should accept this course ID and the function should return the sessions for this course")  
+	  public void thenTheOutcomeShould() {  
+		  assertEquals(true, detailsChecked);
+	  }  
 }
