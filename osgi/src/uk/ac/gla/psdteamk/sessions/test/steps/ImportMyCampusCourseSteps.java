fa8af7e9abe24f0b89f95e01244e5eef2e4f63f0
Tomazz <1106113s@student.gla.ac.uk>
2014-02-27 13:39:47 +0000

    Update ImportMyCampusCourseSteps.java

--- osgi/src/uk/ac/gla/psdteamk/sessions/test/steps/ImportMyCampusCourseSteps.java
+++ osgi/src/uk/ac/gla/psdteamk/sessions/test/steps/ImportMyCampusCourseSteps.java
@@ -1,69 +1,34 @@
 package uk.ac.gla.psdteamk.sessions.test.steps;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import static org.junit.Assert.assertEquals;
+
+import org.jbehave.core.annotations.Given;  
+import org.jbehave.core.annotations.Named;  
+import org.jbehave.core.annotations.Then;  
+import org.jbehave.core.annotations.When;  
 
 import uk.ac.gla.psdteamk.database.service.DatabaseAdapterService;
-import static org.junit.Assert.assertEquals;
-import org.jbehave.core.annotations.Given;
-import org.jbehave.core.annotations.Then;
-import org.jbehave.core.annotations.When;
+import uk.ac.gla.psdteamk.objects.Course;
+import uk.ac.gla.psdteamk.sessions.*;
 import org.jbehave.core.steps.Steps;
 
-
-
-public class NFR_performance2 extends Steps {
-	private DatabaseAdapterService service;
-	private int users;
-	private boolean imported;
+public class ImportMyCampusCourseSteps extends Steps{
+	private boolean importCourses;
+	private DatabaseAdapterService da;
+	private Course myCourse;
+		
+	  @Given("a valid MyCampus course $co")  
+	  public void givenAMyCampusID(String co) {  
+	    this.myCourse =new Course(0, co);
+	  }  
 	
-	@Given("Given a number of users $u")
-	public void givenADatabase(int users) {
-		this.users = users;
-	}
+	  @When("I try to retrieve the course information ")  
+	  public void whenIRetrieveCourseInfo() {  
+	     importCourses = ImportMyCampusCourses.importCourse(da, myCourse);
+	  }  
 	
-	@When("I try to populate the database with the users")
-	public void populateUserDB() {
-		String sql = "INSERT INTO Crap VALUES (?)"; 
-		Connection con = null;
-		PreparedStatement preparedStatement = null;
-
-		try {
-			//Get the database connection.
-			con = service.getConnection();
-			//Prepare the SQL statement.
-			preparedStatement = con.prepareStatement(sql);
-			//Execute the statement and get the result.
-			preparedStatement.execute();
-			for (int i = 0;i<=users;i++){
-				preparedStatement.setDouble(1, Math.random());
-				preparedStatement.execute();
-			}
-			System.out.println("The course has been successfully imported!");
-			imported = true;
-		} catch (SQLException ex) {
-			System.out.println("This course is already imported!");
-		} finally {
-			//Close the connections.
-			try {
-				if (preparedStatement != null){
-					preparedStatement.close();
-				}
-				if (con != null){
-					con.close();
-				}
-			}
-			catch (SQLException ex){
-				System.out.println(ex.getMessage());
-			}
-		}
-		imported =  false;
-	}
-	
-	@Then("I should be able to add 1000 and more of them")
-	public void check() {
-		assertEquals(true,imported);
-	}
-
+	  @Then("a function should accept this course ID and the function should return the sessions for this course")  
+	  public void thenTheOutcomeShould() {  
+		  assertEquals(true, importCourses);
+	  }  
 }
