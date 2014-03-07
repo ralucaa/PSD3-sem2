1825c55568bba08b7691bc1b731a6e3249d6d48a
Vlad Iulian Schnakovszki <shnako@yahoo.com>
2014-03-07 00:45:59 +0000

    Refactored AddSessionToCourse to use the DatabaseAdapter. So simple ...

--- osgi/src/uk/ac/gla/psdteamk/sessions/AddSessionToCourse.java
+++ osgi/src/uk/ac/gla/psdteamk/sessions/AddSessionToCourse.java
@@ -1,7 +1,4 @@
 package uk.ac.gla.psdteamk.sessions;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 
 import uk.ac.gla.psdteamk.database.service.DatabaseAdapterService;
 import uk.ac.gla.psdteamk.objects.*;
@@ -12,41 +9,6 @@ class AddSessionToCourse {
 	 * @param session - The session object to add.
 	 */
 	static boolean addSessionToDatabase(DatabaseAdapterService da, Session session) {
-		//Add to database.
-		String sql = "INSERT INTO \"Session\"(\"course\", \"date\", \"capacity\", \"type\") VALUES (?, ?, ?, ?)"; 
-		Connection con = null;
-		PreparedStatement preparedStatement = null;
-
-		try {
-			//Get the database connection.
-			con = da.getConnection();
-			//Prepare the SQL statement.
-			preparedStatement = con.prepareStatement(sql);
-			//Add the parameters.
-			preparedStatement.setInt(1, session.getCourse());
-			preparedStatement.setString(2, session.getDateString());
-			preparedStatement.setInt(3, session.getCapacity());
-			preparedStatement.setString(4, session.getType());
-			//Execute the statement and get the result.
-			preparedStatement.execute();
-			System.out.println("The session has been successfully imported!");
-			return true;
-		} catch (SQLException ex) {
-			System.out.println(ex.getMessage());
-		} finally {
-			//Close the connections.
-			try {
-				if (preparedStatement != null){
-					preparedStatement.close();
-				}
-				if (con != null){
-					con.close();
-				}
-			}
-			catch (SQLException ex){
-				System.out.println(ex.getMessage());
-			}
-		}
-		return false;
+		return da.addSessionToDatabase(session);
 	}
 }
