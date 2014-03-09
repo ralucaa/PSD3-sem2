f5a6680c88a2026f1a8b50d24496273a3c985e55
Vlad Iulian Schnakovszki <shnako@yahoo.com>
2014-03-09 11:07:08 +0000

    Fixed Tomasz's horrible stories and steps. (fixes #94).

--- osgi/src/uk/ac/gla/psdteamk/sessions/test/steps/CheckCompulsoryCoursesSteps.java
+++ osgi/src/uk/ac/gla/psdteamk/sessions/test/steps/CheckCompulsoryCoursesSteps.java
@@ -6,7 +6,6 @@ import org.jbehave.core.annotations.Given;
 import org.jbehave.core.annotations.Then;
 import org.jbehave.core.annotations.When;
 
-import uk.ac.gla.psdteamk.objects.Account;
 import uk.ac.gla.psdteamk.sessions.service.SessionManagerService;
 import uk.ac.gla.psdteamk.sessions.test.SetupFramework;
 import static org.junit.Assert.*;
@@ -15,37 +14,30 @@ public class CheckCompulsoryCoursesSteps {
 	private SessionManagerService service;
 	boolean output;
 	private int studentToken;
-	private Account student;
 	
 	@BeforeScenario
 	public void beforeScenario() throws Exception {
 		SetupFramework.defaultPopulate();
 		service = SetupFramework.getSessionManagerService();
-		//studentToken = service.authenticate("3333333B", "3333333B");
 	}
 	
 	@AfterScenario
 	public void afterScenario() throws Exception {
 	}
 	
-	@Given("a student username $username")
-	public void aStudentUsername(String username) {
-		studentToken = service.authenticate("33bogus33B", "bogus333B");
-		student = new Account(username, "", "", "", 1);
+	@Given("a student with username $username and password $password")
+	public void aStudentUsername(String username, String password) {
+		studentToken = service.authenticate(username, password);
 	}
 	
-	@When("I check the student in database")
+	@When("the student checks if he is fully registered for the compulsory courses")
 	public void isFalseStudentRegistered() {
 		output = service.checkIfFullyRegistered(studentToken);
 	}
 	
 	@Then("the output of the check is $output")
-	public void thenCheck(boolean output) {
-		if(output == false){
-			assertEquals(this.output, false);
-		}else{
-			assertEquals(this.output, true);
-		}
-		
+	public void thenCheck(String output) {
+		boolean boolOutput = Boolean.parseBoolean(output);
+		assertEquals(boolOutput, this.output);
 	}	
 }
--- osgi/src/uk/ac/gla/psdteamk/sessions/test/stories/CheckCompulsoryCourses.story
+++ osgi/src/uk/ac/gla/psdteamk/sessions/test/stories/CheckCompulsoryCourses.story
@@ -4,11 +4,16 @@ As a student
 I want to check I signed up for everything required
 
 Scenario: student username is not in the database
-Given a student username bogus
-When I check the student in database
+Given a student with username bogususer and password bogususer
+When the student checks if he is fully registered for the compulsory courses
 Then the output of the check is false
 
 Scenario: student checks if is registered for the compulsory courses
-Given a student username 3333333A
-When I check the student in database
+Given a student with username 3333333A and password 3333333A
+When the student checks if he is fully registered for the compulsory courses
+Then the output of the check is false
+
+Scenario: student checks if is registered for the compulsory courses
+Given a student with username 3333333E and password 3333333E
+When the student checks if he is fully registered for the compulsory courses
 Then the output of the check is true
