ba67a6a49c363b2de51804a65069aedb1034b61a
ralucaa <1102430c@student.gla.ac.uk>
2014-03-10 17:08:14 +0000

    Changed NRF_Performance0Steps

--- osgi/src/uk/ac/gla/psdteamk/sessions/test/steps/NFR_Performance0Steps.java
+++ osgi/src/uk/ac/gla/psdteamk/sessions/test/steps/NFR_Performance0Steps.java
@@ -6,41 +6,42 @@ import org.jbehave.core.annotations.BeforeScenario;
 import org.jbehave.core.annotations.Given;
 import org.jbehave.core.annotations.Then;
 import org.jbehave.core.annotations.When;
-
-import uk.ac.gla.psdteamk.database.service.DatabaseAdapterService;
-import uk.ac.gla.psdteamk.objects.Course;
+import uk.ac.gla.psdteamk.objects.Session;
+import uk.ac.gla.psdteamk.sessions.service.SessionManagerService;
 import uk.ac.gla.psdteamk.sessions.test.SetupFramework;
 import static org.junit.Assert.assertEquals;
 
 
 public class NFR_Performance0Steps {
-	private DatabaseAdapterService dbs;
-	String courseName;
-	boolean output = true;
-	
-	@BeforeScenario
-	public void beforeScenario() throws Exception {
-		dbs = SetupFramework.getDatabaseAdapterService();
-	}
-	
-	@AfterScenario
-	public void afterScenario() throws Exception {
-	}
-	
-	@Given("a course name $courseName")
-	public void aStudentUsername(String courseName) {			
-		this.courseName = courseName;
-	}
-	
-	@When("I try to add it a $numOfAdded times to the database")
-	public void queryDatabase(int numOfAdded) {
-		for (int i = 0; i < numOfAdded; i++) {
-			output = (output && dbs.addCourseToDatabase(new Course((i*i + 1337), courseName + i)));
-		}
-	}
-	
-	@Then("the output is $output")
-	public void theOutputIs(boolean output) {
-		assertEquals(this.output, output);
-	}	
+    String courseName;
+    boolean output = true;
+    private SessionManagerService sms;
+    
+    @BeforeScenario
+    public void beforeScenario() throws Exception {
+        sms= SetupFramework.getSessionManagerService();
+    }
+    
+    @AfterScenario
+    public void afterScenario() throws Exception {
+    }
+    
+    @Given("a course name $courseName")
+    public void aStudentUsername(String courseName) {            
+        this.courseName = courseName;
+    }
+    
+    @When("I try to add it a $numOfAdded times to the database")
+    public void queryDatabase(int numOfAdded) {
+        for (int i = 0; i < numOfAdded; i++) {
+            //output = (output && dbs.addCourseToDatabase(new Course((i*i + 1337), courseName + i)));
+            output= (output && sms.addSessionToCourse(i, new Session(1,1,1,courseName)));
+            //output =(output && sms.addSessionToCourse(i, new Session(1,1,1,1,"")));
+        }
+    }
+    
+    @Then("the output is true")
+    public void theOutputIs(boolean output) {
+        assertEquals(this.output, output);
+    }    
 }
