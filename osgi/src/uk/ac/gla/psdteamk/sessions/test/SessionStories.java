6841d34aa796d99336aa2a74a35858c477d43601
HelenFoster <0904275f@student.gla.ac.uk>
2014-03-12 23:20:15 +0000

    Fixed "broken" comments

--- osgi/src/uk/ac/gla/psdteamk/sessions/test/SessionStories.java
+++ osgi/src/uk/ac/gla/psdteamk/sessions/test/SessionStories.java
@@ -1,12 +1,9 @@
 package uk.ac.gla.psdteamk.sessions.test;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 
 import org.jbehave.core.embedder.Embedder;
-import org.jbehave.core.io.StoryFinder;
 import org.junit.Test;
 
 public class SessionStories {
@@ -20,15 +17,15 @@ public class SessionStories {
 		storyPaths.add("uk/ac/gla/psdteamk/sessions/test/stories/AddSessionToCourse.story");
 		storyPaths.add("uk/ac/gla/psdteamk/sessions/test/stories/AssignRoomToTimetableSlot.story");
 		storyPaths.add("uk/ac/gla/psdteamk/sessions/test/stories/BookTimetableSlot.story");
-		storyPaths.add("uk/ac/gla/psdteamk/sessions/test/stories/CheckCompulsoryCourses.story"); // Broken
+		storyPaths.add("uk/ac/gla/psdteamk/sessions/test/stories/CheckCompulsoryCourses.story");
 		storyPaths.add("uk/ac/gla/psdteamk/sessions/test/stories/CheckForClashes.story");
-		storyPaths.add("uk/ac/gla/psdteamk/sessions/test/stories/CheckSessionDetails.story"); // Broken
-		storyPaths.add("uk/ac/gla/psdteamk/sessions/test/stories/CreateTimetableSlotForSession.story"); // Broken
-		storyPaths.add("uk/ac/gla/psdteamk/sessions/test/stories/ImportMyCampusCourse.story"); // Broken
+		storyPaths.add("uk/ac/gla/psdteamk/sessions/test/stories/CheckSessionDetails.story");
+		storyPaths.add("uk/ac/gla/psdteamk/sessions/test/stories/CreateTimetableSlotForSession.story");
+		storyPaths.add("uk/ac/gla/psdteamk/sessions/test/stories/ImportMyCampusCourse.story");
 		storyPaths.add("uk/ac/gla/psdteamk/sessions/test/stories/NFR_Performance0.story");
 		storyPaths.add("uk/ac/gla/psdteamk/sessions/test/stories/NFR_Performance1.story");
-		storyPaths.add("uk/ac/gla/psdteamk/sessions/test/stories/NFR_Performance2.story"); // Broken
-		storyPaths.add("uk/ac/gla/psdteamk/sessions/test/stories/NFR_Performance3.story"); // Broken
+		storyPaths.add("uk/ac/gla/psdteamk/sessions/test/stories/NFR_Performance2.story");
+		storyPaths.add("uk/ac/gla/psdteamk/sessions/test/stories/NFR_Performance3.story");
 		storyPaths.add("uk/ac/gla/psdteamk/sessions/test/stories/NFR_Performance4.story");
 		storyPaths.add("uk/ac/gla/psdteamk/sessions/test/stories/NFR_Security0.story");
 		storyPaths.add("uk/ac/gla/psdteamk/sessions/test/stories/NFR_Security1.story");
