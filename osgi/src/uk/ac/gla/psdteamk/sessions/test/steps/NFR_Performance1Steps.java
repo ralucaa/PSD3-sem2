eac3e042200ff1409200ded0b99df1325b7673a1
ralucaa <1102430c@student.gla.ac.uk>
2014-03-12 17:19:39 +0000

    NFR performance 1 user story fixed: added 10 different course TYPES instead of courses. However, 1/2 tests fails

--- osgi/src/uk/ac/gla/psdteamk/sessions/test/steps/NFR_Performance1Steps.java
+++ osgi/src/uk/ac/gla/psdteamk/sessions/test/steps/NFR_Performance1Steps.java
@@ -24,17 +24,17 @@ public class NFR_Performance1Steps {
 		this.courseId = courseId;
 	}
 	
-	@When("I try to add 10 sessions to this course")
+	@When("I try to add 10 sessions types to this course")
 	public void queryDatabase() {
 		for (int i = 0; i < 10; i++) {
-			output = (output && dbs.addSessionToDatabase(new Session(courseId, 1, 7, "testType")));
+			output = (output && dbs.addSessionToDatabase(new Session(courseId, 1, 7, "testType"+i)));
 		}		 
 	}
 	
 	@Then("the database adding indicator has to be $output")
 	public void theOutputIs(String output) {
 		boolean boolOutput = Boolean.parseBoolean(output);
-		assertEquals(this.output, boolOutput);
+		assertEquals(boolOutput,this.output);
 	}	
 }
 
--- osgi/src/uk/ac/gla/psdteamk/sessions/test/stories/NFR_Performance1.story
+++ osgi/src/uk/ac/gla/psdteamk/sessions/test/stories/NFR_Performance1.story
@@ -3,7 +3,8 @@ In order to fulfill a non functional requirement
 As a system
 I want to support at least 10 sessions per course
 
-Scenario: add 10 sessions to a course 04
+Scenario: add 10 sessions to an invalid courseId 04
 Given a course id 04
-When I try to add 10 sessions to this course
-Then the database adding indicator has to be true
+When I try to add 10 sessions types to this course
+Then the database adding indicator has to be false
+
